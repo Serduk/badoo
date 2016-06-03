a706dfa108d9361be9c669a6a1cf193321775f6a
Serduk <serduksergey90@gmail.com>
2016-06-03 22:48:37 +0300

    create first test

--- src/test/mob/TestRun.java
+++ src/test/mob/TestRun.java
@@ -1,7 +1,20 @@
 package test.mob;
 
+import core.browser.ChromeUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
 /**
  * Created by serdyuk on 6/3/16.
  */
 public class TestRun {
+    WebDriver driver;
+
+    @Test
+    public void startTest() {
+        ChromeUtils chromeBrowser = new ChromeUtils("FRA", 20);
+        this.driver = chromeBrowser.getMobileBrowser();
+
+        driver.get("https://google.com");
+    }
 }
