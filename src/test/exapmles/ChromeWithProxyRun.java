560bc80b4fbc9ad62a9f544b807e441484e487f7
Serduk <serduksergey90@gmail.com>
2016-11-02 23:52:18 +0200

    Add test example for run test proxy

---
+++ src/test/exapmles/ChromeWithProxyRun.java
@@ -0,0 +1,33 @@
+package test.exapmles;
+
+import core.browser.ChromeUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Example, Haw we can init browser with already workable proxy
+ * Created by sergey on 10/25/16.
+ */
+
+public class ChromeWithProxyRun {
+
+    WebDriver driver;
+
+    @Test
+    public void webProxyTest() {
+        ChromeUtils chromeBrowser = new ChromeUtils("USA", 10);
+        driver = chromeBrowser.getWEBWithProxy("USA");
+
+        driver.get("https://2ip.ru");
+    }
+
+    @Test
+    public void mobProxyTest() throws IOException {
+        ChromeUtils chromeBrowser = new ChromeUtils("USA", 10);
+        driver = chromeBrowser.getMobWithProxy("USA");
+
+        driver.get("https://2ip.ru");
+    }
+}
